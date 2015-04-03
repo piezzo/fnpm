af47e9593ce3df64d35e34bfa71b95cfd993cf94
Patrick Pachur <patrick@PatricksMacBook.fritz.box>
2015-04-03 16:16:18 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {}
+
+config.bitcoinrpc = {};
+
+config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
+config.bitcoinrpc.user = "";
+config.bitcoinrpc.password = "";
+
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var prettyjson = require('prettyjson');
+var d3 = require('d3');
+//var d3pie = require('d3pie');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	req.client.cmd('getnettotals', function(err, info, resHeaders){
+	  if (err) return console.log(err);
+	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	  res.render('index', {"data" : info
+	  });
+	  //console.log(info);
+  });
+//  res.render('index', { title: 'Express' });
+});
+
+/*
+router.get('/getinfo', function(req, res, next) {
+	req.client.getBalance('*', 6, function(err, balance, resHeaders) {
+	  if (err) return console.log(err);
+	  console.log('Balance:', balance);
+	});
+  res.render('getinfo', { title: 'Express getinfo' });
+});
+*/
+
+/*
+router.get('/getinfo', function(req, res, next) {
+	req.client.cmd('getinfo', function(err, balance, resHeaders){
+	  if (err) return console.log(err);
+	  console.log('Balance:', balance);
+	});
+  res.render('getinfo', { title: 'Express getinfo' });
+});
+*/
+
+
+router.get('/getinfo', function(req, res, next) {
+	req.client.cmd('getinfo', function(err, info, resHeaders){
+	  if (err) return console.log(err);
+	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	  res.render('getinfo', {"data" : info
+	  });
+//	  console.log(info);
+  });
+ // res.render('getinfo', { title: 'Express getinfo' });
+});
+
+router.get('/getpeerinfo', function(req, res, next) {
+	req.client.cmd('getpeerinfo', function(err, info, resHeaders){
+	  if (err) return console.log(err);
+//	  console.log(info);
+//	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	  res.render('getpeerinfo', {"data" : info
+	 // var options = {
+	 //   noColor: true
+	 // };
+	 // res.render('getinfo', {"data" : prettyjson.render(info, {noColor: true})
+	  });
+//	  console.log(info);
+  });
+ // res.render('getinfo', { title: 'Express getinfo' });
+});
+
+module.exports = router;
