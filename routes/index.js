723387bff3cbe2d241d4f411ef5c43d323dab650
Patrick <patrick.pachur@gmail.com>
2015-04-04 12:48:20 +0200

    cleanup...

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var d3 = require('d3');
+//var d3 = require('d3');
 //var d3pie = require('d3pie');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	req.client.cmd('getnettotals', function(err, info, resHeaders){
-	  if (err) return console.log(err);
-	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	  res.render('index', {"data" : info
-	  });
-	  //console.log(info);
-  });
-//  res.render('index', { title: 'Express' });
+//	req.client.cmd('getnettotals', function(err, info, resHeaders){
+//	  if (err) return console.log(err);
+//	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+//	  res.render('index', {"data" : info
+//	  });
+//	  console.log(info);
+//  });
+	res.render('index', { title: 'Express' })
 });
-
-/*
-router.get('/getinfo', function(req, res, next) {
-	req.client.getBalance('*', 6, function(err, balance, resHeaders) {
-	  if (err) return console.log(err);
-	  console.log('Balance:', balance);
-	});
-  res.render('getinfo', { title: 'Express getinfo' });
-});
-*/
-
-/*
-router.get('/getinfo', function(req, res, next) {
-	req.client.cmd('getinfo', function(err, balance, resHeaders){
-	  if (err) return console.log(err);
-	  console.log('Balance:', balance);
-	});
-  res.render('getinfo', { title: 'Express getinfo' });
-});
-*/
-
 
 router.get('/getinfo', function(req, res, next) {
 	req.client.cmd('getinfo', function(err, info, resHeaders){
@@ -54,10 +33,6 @@ router.get('/getpeerinfo', function(req, res, next) {
 //	  console.log(info);
 //	  var result = JSON.stringify(info).replace(/</g, '&lt;').replace(/>/g, '&gt;');
 	  res.render('getpeerinfo', {"data" : info
-	 // var options = {
-	 //   noColor: true
-	 // };
-	 // res.render('getinfo', {"data" : prettyjson.render(info, {noColor: true})
 	  });
 //	  console.log(info);
   });
