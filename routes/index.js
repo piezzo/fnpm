d8b0214324fdd9eb935ad56da915db8e714ce871
Patrick <patrick.pachur@gmail.com>
2015-04-13 00:37:24 +0200

    was broken... quickfix

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var rpcConfig = config.get('rpcConfig');
 var colors = config.get('colors');
 
 //load btcd and config
-var btcd = require('btcd')('wss://' + rpcConfig.btcd.user + ':' + rpcConfig.btcd.password + '@localhost:8334/ws',
-                           __dirname + '/rpc.cert');
+//var btcd = require('btcd')('wss://' + rpcConfig.btcd.user + ':' + rpcConfig.btcd.password + '@localhost:8334/ws',
+//                           __dirname + '/rpc.cert');
 
 
 /* GET home page. */
@@ -41,25 +41,49 @@ router.get('/getpeerinfo', function(req, res, next) {
 		case 'btcd':
 			btcd.getpeerinfo(function(err, info, resHeaders) {
 				if (err) return console.log(err);
+				res.render('getpeerinfo', {
+					"data": info,
+					"colors": colors,
+					"nettotals": nettotals
+				});
 			});
-			btcd.getnettotals(function(err, nettotals, resHeaders) {
-				if (err) return console.log(err);
-			});
+			// btcd.getnettotals(function(err, nettotals, resHeaders) {
+			// 	if (err) return console.log(err);
+			// });
 			break;
 		case 'bitcoind':
 			req.client.cmd('getpeerinfo', function(err, info, resHeaders) {
 				if (err) return console.log(err);
+				console.log(info);
+				console.log(colors);
+				console.log(nettotals);
+				res.render('getpeerinfo', {
+					"data": info,
+					"colors": colors,
+					"nettotals": nettotals
+				});
 			});
-			req.client.cmd('getnettotals', function(err, nettotals, resHeaders) {
-				if (err) return console.log(err);
+			// req.client.cmd('getnettotals', function(err, nettotals, resHeaders) {
+			// 	if (err) return console.log(err);
+			// 	console.log(info);
+			// 	console.log(colors);
+			// 	console.log(nettotals);
+				// res.render('getpeerinfo', {
+				// 	"data": info,
+				// 	"colors": colors,
+				// 	"nettotals": nettotals
+				// });
 			});
+			// var nettotals = config.rpcConfig.sampleData.getnettotals;
+
 			break;
 		case 'sampledata':
 			{
+				console.log("acessing sampledata...");
 				var info = config.rpcConfig.sampleData.getpeerinfo;
 				var nettotals = config.rpcConfig.sampleData.getnettotals;
 			};
-	console.log("serverType: " + serverType + "info: " + info);
+	console.log("serverType: " + serverType + ", info: " + info);
 	res.render('getpeerinfo', {
 		"data": info,
 		"colors": colors,
